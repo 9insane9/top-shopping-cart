77d356ada208a56e70b5c45fe83150dda13d8ad8
9insane9 <145199591+9insane9@users.noreply.github.com>
2025-04-03 15:37:53 +0300

    create component files

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+function Nav() {
+  return (
+    <nav>
+      <ul className="links">
+        {/* <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Store</Link>
+        </li> */}
+      </ul>
+    </nav>
+  )
+}
