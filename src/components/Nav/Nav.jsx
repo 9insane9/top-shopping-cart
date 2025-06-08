eff96e500a7187a46b922e3371b2544002b5174a
9insane9 <145199591+9insane9@users.noreply.github.com>
2025-06-08 20:16:03 +0300

    styling tweaks

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -36,50 +36,52 @@ export default function Nav({ onDrawerToggle, setDrawerContent }) {
   }
 
   return (
-    <nav className={classes.navBar}>
-      <ul className={classes.links}>
-        <li>
-          <Link to="home">Home</Link>
-        </li>
-        <li>
-          <Link to="shop" onClick={handleShopClick}>
-            Shop
-          </Link>
-        </li>
-      </ul>
-      <div className={classes.btnContainer}>
-        <form className={classes.searchContainer} onSubmit={handleSubmit}>
-          <input
-            className={classes.searchInput}
-            type="search"
-            name="search"
-            id="search"
-            defaultValue={searchTerm}
-            placeholder="Search games..."
-          />
-          <button type="submit" className={classes.btn}>
+    <div className={classes.navBarContainer}>
+      <nav className={classes.navBar}>
+        <ul className={classes.links}>
+          <li>
+            <Link to="home">Home</Link>
+          </li>
+          <li>
+            <Link to="shop" onClick={handleShopClick}>
+              Shop
+            </Link>
+          </li>
+        </ul>
+        <div className={classes.btnContainer}>
+          <form className={classes.searchContainer} onSubmit={handleSubmit}>
+            <input
+              className={classes.searchInput}
+              type="search"
+              name="search"
+              id="search"
+              defaultValue={searchTerm}
+              placeholder="Search games..."
+            />
+            <button type="submit" className={classes.btn}>
+              {icons.search({ className: `${classes.search}` })}
+            </button>
+          </form>
+          <button
+            className={`${classes.btn} ${classes.searchBtn}`}
+            onClick={handleSearchClick}
+          >
             {icons.search({ className: `${classes.search}` })}
           </button>
-        </form>
-        <button
-          className={`${classes.btn} ${classes.searchBtn}`}
-          onClick={handleSearchClick}
-        >
-          {icons.search({ className: `${classes.search}` })}
-        </button>
-        <button
-          className={`${classes.btn} ${classes.cartBtn}`}
-          onClick={handleCartClick}
-        >
-          {icons.cart({ className: `${classes.cart}` })}
-          {cartTotalQty ? (
-            <p className={classes.tinyNumber}>{cartTotalQty}</p>
-          ) : null}
-        </button>
-        {/* <button className={classes.btn} onClick={handleMenuClick}>
+          <button
+            className={`${classes.btn} ${classes.cartBtn}`}
+            onClick={handleCartClick}
+          >
+            {icons.cart({ className: `${classes.cart}` })}
+            {cartTotalQty ? (
+              <p className={classes.tinyNumber}>{cartTotalQty}</p>
+            ) : null}
+          </button>
+          {/* <button className={classes.btn} onClick={handleMenuClick}>
           <img src="/menu.svg" alt="menu" />
         </button> */}
-      </div>
-    </nav>
+        </div>
+      </nav>
+    </div>
   )
 }
